3f5ae51dccb6b90114de23f9e7d21971d211c4f5
Snacks-on-fire <dalia.k.m@gmail.com>
2021-07-23 13:54:14 +0300

    Initial Commit

---
+++ M3-L2 LearningGH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M3-L2 LearningGH
+//
+//  Created by Dalia Maraoulaite on 7/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M3-L2 LearningGH/M3_L2_LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  M3_L2_LearningGHApp.swift
+//  M3-L2 LearningGH
+//
+//  Created by Dalia Maraoulaite on 7/23/21.
+//
+
+import SwiftUI
+
+@main
+struct M3_L2_LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
